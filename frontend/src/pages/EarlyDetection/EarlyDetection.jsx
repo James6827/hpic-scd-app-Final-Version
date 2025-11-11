53433cf3c2a5b6ff81f572a4bd3245a10de7f78e
abbywilson11 <awils141@uottawa.ca>
2025-11-11 14:43:55 -0500

    added rror handling for pages

---
+++ frontend/src/pages/EarlyDetection/EarlyDetection.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Community() {
+  return (
+    <div className="early-detection-container">
+      <h1>Early Detection Page</h1>
+      <p>Welcome to the HPIC early detection resources section.</p>
+    </div>
+  );
+}
---
+++ frontend/src/pages/Education/Education.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Community() {
+  return (
+    <div className="education-container">
+      <h1>Education Page</h1>
+      <p>Welcome to the HPIC education resources section.</p>
+    </div>
+  );
+}
