6f931c4957f4a4164fcc2217a0feb50fe2406ed7
abbywilson11 <awils141@uottawa.ca>
2025-11-13 15:25:06 -0500

    reorganized format of info section

--- frontend/src/pages/HomePage/Home.jsx
+++ frontend/src/pages/HomePage/Home.jsx
@@ -24,14 +24,6 @@ export default function Home() {
           <img src={ribbon} alt="Awareness Ribbon" /> {/*image for the right side*/}
         </div>
       </section>
-
-      <section className="info-section">
-        <h2>How HPIC Can Help</h2>
-        <p>
-         We offer many tools and resources for you to discover 
-         how to take care of yourself and your loved ones who are affected by Sickle Cell Disease.
-        </p>
-      </section>
     </div>
   );
 }
---
+++ frontend/src/pages/Information section/info-section.jsx
@@ -0,0 +1,13 @@
+import "./info-section.css";
+
+export default function InfoSection() {
+  return (    
+      <section className="info-section">
+        <h2>How HPIC Can Help</h2>
+        <p>
+         We offer many tools and resources for you to discover 
+         how to take care of yourself and your loved ones who are affected by Sickle Cell Disease.
+        </p>
+      </section>
+    );
+}
