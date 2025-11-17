ff310c3fa8e10f168c38d90cd1df16b8bdfc05d0
abbywilson11 <awils141@uottawa.ca>
2025-11-17 11:58:18 -0500

    updated css

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,34 +1,45 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./Navbar.css";
-import logo from "../assets/Logo.png"; // path to logo image
+import logo from "../assets/Logo.png";
 
 function Navbar() {
   const navigate = useNavigate();
 
   return (
     <nav className="navbar">
-      <div className="navbar-left"> {/* positioning of logo on page */}
-        <img src={logo} alt="HPIC Logo" className="logo" onClick={() => navigate("/")} style={{ cursor: "pointer" }}/> {/* HPIC logo image */}
+      {/* Left: Logo */}
+      <div className="navbar-left">
+        <img
+          src={logo}
+          alt="HPIC Logo"
+          className="logo"
+          onClick={() => navigate("/")}
+          style={{ cursor: "pointer" }}
+        />
       </div>
 
-      {/* what is a button in  centre of navigation section of app */}
+      {/* Center: Page Buttons */}
       <div className="navbar-center">
         <button onClick={() => navigate("/symptoms")}>Symptoms</button>
         <button onClick={() => navigate("/treatment")}>Treatment</button>
         <button onClick={() => navigate("/myjournal")}>My Journal</button>
       </div>
-        
-      {/* right ish side of navigation bar for language options*/}
- <div className="navbar-leftright">
-        <button onClick={() => navigate("/ENFR")}>Language</button>
-      </div>
 
-      {/* right side of navigation bar for sign in / sign up options*/}
-      <div className="navbar-right">
-        <button onClick={() => navigate("/login")}>Login / Sign Up</button>
+      {/* Right: Language + Login (properly grouped) */}
+      <div className="navbar-end">
+        <div className="navbar-leftright">
+          <button className="language-btn" onClick={() => navigate("/ENFR")}>
+            EN / FR
+          </button>
+        </div>
+
+        <button className="login-btn" onClick={() => navigate("/login")}>
+          Login / Sign Up
+        </button>
       </div>
     </nav>
   );
 }
+
 export default Navbar;
